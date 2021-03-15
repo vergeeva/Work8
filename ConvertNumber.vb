a7dcd58d06710c6e4de86897d06fca1273f6147b
nastya <nastya.vergeeva@mail.ru>
2021-03-15 12:09:59 +0500

    Добавьте файлы проекта.

---
+++ ConvertNumber.vb
@@ -0,0 +1,41 @@
+﻿Public Class ConvertNumber
+    Private Number As Integer
+    Private ReadOnly Arabian() As Integer = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+    Private ReadOnly Roman() As String = {"M", "CM", "D", "CD", "С", "ХС", "L", "XL", "X", "IX", "V", "IV", "I"}
+
+    Public Sub New(Number As Integer)
+        If CheckNumber(Number) Then
+            Me.Number = Number
+        Else Me.Number = 1
+        End If
+    End Sub
+    Public Property Numbr() As Integer
+        Get
+            Return Number
+        End Get
+        Set(value As Integer)
+            If CheckNumber(value) Then
+                Number = value
+            End If
+        End Set
+    End Property
+
+    Private Function CheckNumber(Number As Integer) As Boolean
+        Return Number > 0 And Number <= 3999
+    End Function
+
+    Public Function ConvertToRoman() As String
+        Dim result As String = ""
+        While Number > 0
+            For index = 0 To Arabian.Count - 1
+                If Arabian(index) <= Number Then
+                    While Arabian(index) <= Number
+                        result += Roman(index)
+                        Number -= Arabian(index)
+                    End While
+                End If
+            Next
+        End While
+        Return result
+    End Function
+End Class
---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim Numbr As ConvertNumber
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Value As Integer
+        If TextBox1.Text <> "" Then
+            Value = Convert.ToInt32(TextBox1.Text)
+            If Value > 0 And Value <= 3999 Then
+                Numbr = New ConvertNumber(Value)
+                TextBox2.Text = Numbr.ConvertToRoman()
+            Else MessageBox.Show("Число выходит за пределы диапазона от 1 до 3999", "Сообщение об ошибке")
+            End If
+        Else
+            MessageBox.Show("Вы не ввели число", "Сообщение об ошибке")
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not (Char.IsControl(e.KeyChar)) And Not (Char.IsDigit(e.KeyChar)) And (e.KeyChar <> ".") Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
